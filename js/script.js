e39d942b290239c2bad2bd574b9da090404229d1
jplourd <jeffplourd@gmail.com>
2015-02-02 13:47:48 -0800

    Added google streetview to document.

--- js/script.js
+++ js/script.js
@@ -12,6 +12,13 @@ function loadData() {
     $nytElem.text("");
 
     // load streetview
+    var streetLocation = $("#street").val();
+    var cityLocation = $("#city").val();
+    var fullLocation = streetLocation + ", " + cityLocation;
+    var bgImageTemp = '<img class="bgimg" src="https://maps.googleapis.com/maps/api/streetview?size=600x400&location=%data%">';
+    var bgImage = bgImageTemp.replace("%data%", fullLocation);
+    $body.append(bgImage);
+
 
     // YOUR CODE GOES HERE!
 
